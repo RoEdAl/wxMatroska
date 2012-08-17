f6b2a0deb5b31d142b6b7830d30b7e5ac8156595
RoEdAl <roed@onet.eu>
2012-08-17 11:22:46 +0200

    TagLib: First steps

--- wxWaw2Img/wxApp.cpp
+++ wxWaw2Img/wxApp.cpp
@@ -1054,6 +1054,9 @@ static void html_renderer()
 	img.SaveFile( "C:/Users/Normal/Documents/Visual Studio 2010/Projects/wxMatroska/html_render.png" );
 }
 
+#include <tpropertymap.h>
+#include <fileref.h>
+
 int wxMyApp::OnRun()
 {
 	switch( m_cfg.GetInfoSubject() )
@@ -1077,6 +1080,21 @@ int wxMyApp::OnRun()
 		case wxConfiguration::INFO_SYSTEM_SETTINGS:
 		InfoSystemSettings( *wxMessageOutput::Get() );
 		return 0;
+	}
+
+	{
+		TagLib::FileRef f( m_cfg.GetInputFile().GetFullPath().t_str() );
+		const TagLib::PropertyMap& props = f.file()->properties();
+		for( TagLib::PropertyMap::ConstIterator i = props.begin(), end = props.end(); i != end; ++i )
+		{
+			for( TagLib::StringList::ConstIterator j = i->second.begin(), jend = i->second.end(); j != jend; ++j )
+			{
+				wxLogDebug( wxT("TAG(%s): %s"), i->first.toWString(), j->toWString() );
+			}
+		}
+
+		TagLib::AudioProperties const *aprops = f.audioProperties();
+		aprops->
 	}
 
 	//html_renderer();
